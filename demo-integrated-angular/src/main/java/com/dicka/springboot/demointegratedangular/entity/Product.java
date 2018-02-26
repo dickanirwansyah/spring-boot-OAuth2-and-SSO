5fcac85e901fae3e71b441205a959ff368057acb
dickanirwansyah <dickanirwansyah@gmail.com>
2018-02-26 13:12:05 +0700

    getter setter product

--- demo-integrated-angular/src/main/java/com/dicka/springboot/demointegratedangular/entity/Product.java
+++ demo-integrated-angular/src/main/java/com/dicka/springboot/demointegratedangular/entity/Product.java
@@ -41,4 +41,68 @@ public class Product implements Serializable{
     @JsonIgnore
     @OneToMany(mappedBy = "product", fetch = FetchType.EAGER)
     private List<OrdersDetils> ordersDetils;
+
+    public String getIdproduct(){
+        return idproduct;
+    }
+
+    public void setIdproduct(String idproduct){
+        this.idproduct = idproduct;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getImage(){
+        return image;
+    }
+
+    public void setImage(String image){
+        this.image = image;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    public boolean isValid(){
+        return valid;
+    }
+
+    public void setValid(boolean valid){
+        this.valid = valid;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category){
+        this.category=category;
+    }
+
+    public List<OrdersDetils> getOrdersDetils(){
+        return ordersDetils;
+    }
+
+    public void setOrdersDetils(List<OrdersDetils> ordersDetils){
+        this.ordersDetils = ordersDetils;
+    }
 }
